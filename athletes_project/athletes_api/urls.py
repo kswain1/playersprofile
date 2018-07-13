4203d8ec005e2f69f4eb8cdb115c15903df8610f
kehlin swain <kehlinswain@kehlins-MacBook-Pro.local>
2018-07-13 15:47:29 -0500

    updates on url views

--- athletes_project/athletes_api/urls.py
+++ athletes_project/athletes_api/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url 
 from . import views
 
+#it's going to render this result for the api review
+#endpoint is going to be hello-view, and it's going to render the as_view
 
-url patterns = [
+urlpatterns = [
 	url(r'^hello-view/', views.HelloApiView.as_view()),
+
 ]
