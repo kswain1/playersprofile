6d8eafff5acbb144326de03dc8ad8c51e607eb64
kehlin swain <kehlinswain@kehlins-MacBook-Pro.local>
2018-07-28 17:54:37 -0500

    added permissions to the project

--- athletes_project/athletes_api/views.py
+++ athletes_project/athletes_api/views.py
@@ -4,9 +4,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from . import serializer
 from . import models
+from . import permissions
 
 # Create your views here.
 
@@ -115,6 +117,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
 	serializer_class = serializer.UserProfileSerializer
 	queryset = models.UserProfile.objects.all()
+	authentication_classes = (TokenAuthentication,)
+	permission_classes = (permissions.UpdateOwnProfile,)
+
+
 
 
 
